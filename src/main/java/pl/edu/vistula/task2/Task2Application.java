eb240f9f8628a1eb5dbbfb31818d68d0f5df2c27
Israel kwatatshey <mukandaisrael54@gmail.com>
2026-01-15 08:46:30 +0100

    Fix formatting in Task2Application.java

--- src/main/java/pl/edu/vistula/task2/Task2Application.java
+++ src/main/java/pl/edu/vistula/task2/Task2Application.java
@@ -3,7 +3,7 @@ package pl.edu.vistula.task2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class Task2Application {
 
 	public static void main(String[] args) {
